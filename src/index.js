b4b3e655da3b6e6d32cd01ada16b8a981d204f75
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-11 21:09:31 +0530

    [chore] Chakra UI installed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
