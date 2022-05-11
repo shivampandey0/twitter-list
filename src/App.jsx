67a815dea30026f51cb4038aee4a6560084ded70
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-12 02:09:59 +0530

    [feat] authstate listener and routes based on auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
+import { onAuthStateChanged } from "firebase/auth";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Footer } from "./components";
+import { Footer, Navbar } from "./components";
+import { useAuth } from "./context/auth-context/Context";
 import { Authenticate, Error, Home } from "./pages";
+import { Lists } from "./pages/Lists";
+import { RedirectAuth } from "./router/RedirectAuth";
+import { RequireAuth } from "./router/RequireAuth";
+import { auth } from "./utils/firebase-config";
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+
+  React.useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        localStorage.setItem("loggedIn", true);
+        setIsLoggedIn(true);
+      } else {
+        localStorage.setItem("loggedIn", false);
+        setIsLoggedIn(false);
+      }
+    });
+
+    return () => unsub;
+  }, [isLoggedIn]);
+
   return (
     <>
+      {isLoggedIn && <Navbar />}
       <Routes>
         <Route path="*" element={<Error />} />
         <Route path="/" element={<Home />} />
-        <Route path="/authenticate" element={<Authenticate />} />
+        <Route element={<RedirectAuth />}>
+          <Route path="/authenticate" element={<Authenticate />} />
+        </Route>
+
+        <Route element={<RequireAuth />}>
+          <Route path="/lists" element={<Lists />} />
+        </Route>
       </Routes>
       <Footer />
     </>
