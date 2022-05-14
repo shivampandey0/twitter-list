2b8e9061a07511c3003d640aa4eb6e1c68320ffb
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-15 02:10:28 +0530

    [chore] utils methods

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+const openProfile = (username) =>
+  window.open(`https://twitter.com/${username}`, '_blank');
+
+const openGithub = () =>
+  window.open(`https://github.com/shivampandey0`, '_blank');
+
+const openTwitter = () =>
+  window.open(`https://twitter.com/ErShivamPandey`, '_blank');
+
+export { openProfile, openGithub, openTwitter };
