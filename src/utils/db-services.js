968925c52fafe9742ed93776cf28e8eb86a54819
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-15 02:10:10 +0530

    [feat] firestore methods

--- src/utils/db-services.js
+++ src/utils/db-services.js
@@ -1,4 +1,4 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc } from 'firebase/firestore';
 import { db } from './firebase-config';
 
 // Firestore methods
@@ -6,6 +6,39 @@ export const addDataToFirestore = async (uid, data) => {
   try {
     const docRef = collection(db, 'twitter_lists', uid, 'lists');
     await addDoc(docRef, data);
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+export const addUserInList = async (uid, id, data) => {
+  try {
+    const colRef = collection(
+      db,
+      'twitter_lists',
+      uid,
+      'lists',
+      id,
+      'twitter_users'
+    );
+    await addDoc(colRef, data);
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+export const deleteUserInList = async (uid, id, docId) => {
+  try {
+    const docRef = doc(
+      db,
+      'twitter_lists',
+      uid,
+      'lists',
+      id,
+      'twitter_users',
+      docId
+    );
+    await deleteDoc(docRef);
   } catch (e) {
     throw new Error(e);
   }
