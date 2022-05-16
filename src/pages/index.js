acf35d6481a5aa91ff635bf061efe0055280a370
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-17 01:22:56 +0530

    [chore][refactor] removed unused files, error page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
-export { Home } from "./Home";
-export { Error } from "./Error";
-export { Authenticate } from "./auth/Auth";
+export { Home } from './Home';
+export { Error } from './Error';
+export { Authenticate } from './auth/Auth';
+export { Lists } from './Lists/Lists';
+export { SingleList } from './SingleList/SingleList';
