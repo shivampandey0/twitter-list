d4ccb4525b587a0bf8f07f8444a8382b6e94622a
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-18 16:45:05 +0530

    [fix] removed commented code

--- src/pages/Lists/SearchBox.jsx
+++ src/pages/Lists/SearchBox.jsx
@@ -1,18 +1,13 @@
 import { Box, Input } from '@chakra-ui/react';
-import React from 'react';
 
-export const SearchBox = ({  onChange }) => {
+export const SearchBox = ({ onChange }) => {
   return (
     <Box paddingX={3} paddingY={2}>
       <Input
         placeholder='Search Lists by name...'
         onChange={(e) => onChange(e.target.value.toLowerCase())}
         required
-        
         backgroundColor='blue.100'
-        // onKeyDown={(e) => {
-        //   if (e.key === 'Enter') searchUser();
-        // }}
       />
     </Box>
   );
